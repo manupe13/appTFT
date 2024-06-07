1a774bb1590b09f46e0678299165151e197c40aa
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-06-07 12:32:49 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { RegisterComponent } from './pages/register/register.component';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyBffqDLZ5MsRXYMSi7zUCtj1poaaIIAFSk",
+    authDomain: "tft1-db11c.firebaseapp.com",
+    projectId: "tft1-db11c",
+    storageBucket: "tft1-db11c.appspot.com",
+    messagingSenderId: "384950390446",
+    appId: "1:384950390446:web:7cea608069b29836f0a73f",
+    measurementId: "G-XWF3R3YVF6"
+  },
+  production: false
+};
