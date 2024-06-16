311e0b5cd5f70873c56fd87103ec1e1b88729c8f
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-06-16 01:30:47 +0100

    Creación de interfaces de ingredientes y recetas

---
+++ src/app/interfaces/ingredient.ts
@@ -0,0 +1,7 @@
+export interface Ingredient {
+  nombre?: string,
+  imgs?: string[],
+  descripcion?: string,
+  filtro?: string,
+  id?: string,
+}
---
+++ src/app/interfaces/recipe.ts
@@ -0,0 +1,11 @@
+export interface Recipe {
+  nombre?: string,
+  imgs?: string[],
+  personas?: string,
+  duracion?: string,
+  dificultad?: string,
+  filtro?: string,
+  ingredientes?: string[],
+  preparacion?: string,
+  id?: string,
+}
