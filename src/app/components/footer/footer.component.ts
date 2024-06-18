8411bbd754626fb1cb62fbdefbd1f1962f0c6afe
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-06-18 09:44:43 +0100

    Arreglo del footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,34 +7,6 @@ import { Component, HostListener } from '@angular/core';
 })
 export class FooterComponent {
 
-  isFooterVisible = false;
-
   constructor() { }
-
-  @HostListener("window:scroll", [])
-  onWindowScroll() {
-    const scrollPosition = window.pageYOffset;
-    const windowHeight = window.innerHeight;
-
-    const body = document.body;
-    const html = document.documentElement;
-    const pageHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-
-    if (scrollPosition + windowHeight >= pageHeight) {
-      if (!this.isFooterVisible) {
-        const footer = document.getElementById('footer');
-        footer?.classList.remove('footer-hide');
-        footer?.classList.add('footer-show');
-        this.isFooterVisible = true;
-      }
-    } else {
-      if (this.isFooterVisible) {
-        const footer = document.getElementById('footer');
-        footer?.classList.add('footer-hide');
-        footer?.classList.remove('footer-show');
-        this.isFooterVisible = false;
-      }
-    }
-  }
 
 }
