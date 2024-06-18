d254cc9bf2d92a2145388da9e21f6556cff02b56
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-06-18 14:01:08 +0100

    Intro y contacta con nosotros

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GlobalDataService } from 'src/app/services/global-data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  loggedUserId: string = 'null';
+
+  constructor(private globalData: GlobalDataService) { }
+
+  ngOnInit(): void {
+    this.globalData.getLoggedUserId().subscribe(id => {
+      this.loggedUserId = id;
+    });
+  }
 
 }
