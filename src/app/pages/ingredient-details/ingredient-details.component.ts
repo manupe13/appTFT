3530be2be36512114b5ae35a962653aff4352253
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-06-19 22:37:05 +0100

    Detalles de ingredientes hecho

--- src/app/pages/ingredient-details/ingredient-details.component.ts
+++ src/app/pages/ingredient-details/ingredient-details.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { Ingredient } from 'src/app/interfaces/ingredient';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IngredientService } from 'src/app/services/ingredient.service';
+import { Ingredient } from 'src/app/interfaces/ingredient';
 import { Storage, ref, uploadBytes, getDownloadURL } from '@angular/fire/storage';
 
 declare var $: any;
@@ -14,25 +14,15 @@ declare var $: any;
 })
 export class IngredientDetailsComponent implements OnInit {
 
-  ingredient: Ingredient = {};
-
-  private initialIngredientData: Ingredient = {};
-
   formIngredient!: FormGroup;
+  ingredient: Ingredient = {};
   selectedFile: File | null = null;
-  ingredientId: string = '';
-
   editable: boolean = false;
+  initialIngredientData: Ingredient = {};
 
   descriptionPattern = /^(?=.*[A-Za-zÁáÉéÍíÓóÚúÜüÑñ])[A-Za-zÁáÉéÍíÓóÚúÜüÑñ\s0-9]{3,}$/;
 
-  constructor(
-    private ingredientService: IngredientService,
-    private router: Router,
-    private fb: FormBuilder,
-    private storage: Storage,
-    private route: ActivatedRoute
-  ) { }
+  constructor(private ingredientService: IngredientService, private router: Router, private route: ActivatedRoute, private fb: FormBuilder, private storage: Storage) { }
 
   ngOnInit(): void {
     this.formIngredient = this.fb.group({
@@ -41,22 +31,72 @@ export class IngredientDetailsComponent implements OnInit {
       filter: ['', [Validators.required]]
     });
 
-    this.route.queryParams.subscribe(params => {
-      this.ingredientId = params['id'];
-      if (this.ingredientId) {
-        this.ingredientService.getIngredientById(this.ingredientId).subscribe((ingredientData: Ingredient) => {
-          this.ingredient = ingredientData;
-          this.initialIngredientData = { ...ingredientData };
-          this.formIngredient.patchValue({
-            img: this.ingredient.imgs,
-            description: this.ingredient.descripcion,
-            filter: this.ingredient.filtro
-          });
-        },
-        (error) => {
-          console.log(error);
+    const id = this.route.snapshot.queryParamMap.get('id');
+    if (id) {
+      this.ingredientService.getIngredientById(id).subscribe((ingredient: Ingredient) => {
+        this.ingredient = ingredient;
+        this.initialIngredientData = { ...ingredient };
+        this.formIngredient.patchValue({
+          img: ingredient.imgs ? ingredient.imgs[0] : '',
+          description: ingredient.descripcion,
+          filter: ingredient.filtro
         });
-      }
+      });
+    }
+  }
+
+  setEditable(): void {
+    this.editable = !this.editable;
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      this.formIngredient.patchValue({ img: file.name });
+      this.formIngredient.get('img')?.updateValueAndValidity();
+    }
+  }
+
+  async update() {
+    if (this.formIngredient.invalid) {
+      this.formIngredient.markAllAsTouched();
+      return;
+    }
+
+    if (this.selectedFile) {
+      const filePath = `ingredientes/${this.selectedFile.name}`;
+      const storageRef = ref(this.storage, filePath);
+      const snapshot = await uploadBytes(storageRef, this.selectedFile);
+      const downloadURL = await getDownloadURL(snapshot.ref);
+      this.ingredient.imgs = [downloadURL];
+    }
+
+    this.ingredient.descripcion = this.formIngredient.value.description;
+    this.ingredient.filtro = this.formIngredient.value.filter;
+
+    this.ingredientService.updateIngredient(this.ingredient.id!, this.ingredient).then(() => {
+      this.setEditable();
+    });
+  }
+
+  cancel() {
+    this.formIngredient.reset({
+      img: this.initialIngredientData.imgs ? this.initialIngredientData.imgs[0] : '',
+      description: this.initialIngredientData.descripcion,
+      filter: this.initialIngredientData.filtro
+    });
+    this.editable = false;
+  }
+
+  confirmDelete() {
+    $('#deleteModal').modal('show');
+  }
+
+  deleteIngredient() {
+    this.ingredientService.deleteIngredient(this.ingredient.id!).then(() => {
+      $('#deleteModal').modal('hide');
+      this.router.navigate(['/ingredients']);
     });
   }
 
@@ -70,85 +110,5 @@ export class IngredientDetailsComponent implements OnInit {
 
   get filterNoValido() {
     return this.formIngredient.get('filter')?.invalid && this.formIngredient.get('filter')?.touched;
-  }
-
-  onFileSelected(event: any) {
-    const file: File = event.target.files[0];
-    if (file) {
-      this.selectedFile = file;
-      this.formIngredient.patchValue({ img: file.name });
-      this.formIngredient.get('img')?.updateValueAndValidity();
-    }
-  }
-
-  setEditable(): void {
-    this.editable = !this.editable;
-  }
-
-  update() {
-    if (this.formIngredient.invalid) {
-      this.formIngredient.markAllAsTouched();
-      console.log("El formulario no es válido");
-      return;
-    }
-
-    const updateData: Ingredient = {
-      ...this.formIngredient.value,
-      imgs: this.formIngredient.value.img
-    };
-
-    if (this.selectedFile) {
-      const imgRef = ref(this.storage, `ingredients/${this.selectedFile.name}`);
-      uploadBytes(imgRef, this.selectedFile).then(snapshot => {
-        getDownloadURL(snapshot.ref).then(downloadURL => {
-          updateData.imgs = [downloadURL];
-          this.ingredientService.updateIngredient(this.ingredientId, updateData).then(updated => {
-            if (updated) {
-              console.log("Se ha actualizado");
-              this.setEditable();
-            } else {
-              console.log("No se ha actualizado");
-            }
-          });
-        });
-      });
-    } else {
-      this.ingredientService.updateIngredient(this.ingredientId, updateData).then(updated => {
-        if (updated) {
-          console.log("Se ha actualizado");
-          this.setEditable();
-        } else {
-          console.log("No se ha actualizado");
-        }
-      });
-    }
-  }
-
-  cancel() {
-    this.formIngredient.reset({
-      img: this.initialIngredientData.imgs,
-      description: this.initialIngredientData.descripcion,
-      filter: this.initialIngredientData.filtro
-    });
-    this.setEditable();
-  }
-
-  confirmDelete() {
-    $('#deleteModal').modal('show');
-  }
-
-  deleteIngredient() {
-    this.ingredientService.deleteIngredient(this.ingredientId).then(deleted => {
-      if (deleted) {
-        console.log("Ingrediente eliminado correctamente");
-        this.router.navigate(['/ingredients']);
-      } else {
-        console.log("No se pudo eliminar el ingrediente");
-      }
-    }).catch((error) => {
-      console.error(error);
-    }).finally(() => {
-      $('#deleteModal').modal('hide');
-    });
   }
 }
